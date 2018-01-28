92b5d455f80839e7b976a16d8b9368dbad03f1e6
serw2203 <serw2203@yandex.ru>
2018-01-28 13:04:19 +0300

    first start

--- src/main/java/ru/desinger/business/BusinessApplication.java
+++ src/main/java/ru/desinger/business/BusinessApplication.java
@@ -1,12 +1,20 @@
 package ru.desinger.business;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(
+        exclude = {DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        }
+)
 public class BusinessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BusinessApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
+    }
 }
--- src/test/java/ru/desinger/business/BusinessApplicationTests.java
+++ src/test/java/ru/desinger/business/BusinessApplicationTests.java
@@ -1,15 +1,15 @@
 package ru.desinger.business;
 
-import org.junit.Test;
+/*import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest*/
 public class BusinessApplicationTests {
 
-	@Test
+	//@Test
 	public void contextLoads() {
 	}
 
